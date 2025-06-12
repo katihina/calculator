4d0ef2f7e24f026f1de3e71e9da3b7288236c2cd
katihina <ognevuschka@gmail.com>
2025-06-12 15:08:24 +0700

    Function division

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,5 +7,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Calculator App");
+        
+        Console.WriteLine($"Деление: {Divide(a, b)}");
+    }
+    
+    static double Divide(int a, int b)
+    {
+        if (b == 0) throw new DivideByZeroException();
+        return (double)a / b;
     }
 }
