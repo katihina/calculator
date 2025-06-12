5a9ad5d8595d28afc8c89d4eeed600f6bf6a4bda
katihina <ognevuschka@gmail.com>
2025-06-12 14:59:38 +0700

    Function addition

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,5 +7,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Calculator App");
+        
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"Add: {Add(a, b)}");
     }
+    
+    static int Add(int a, int b) => a + b;
 }
