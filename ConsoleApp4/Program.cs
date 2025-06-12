b65011e5c283b7b1d4d8c2fd1c23b5c224b1a5e2
katihina <ognevuschka@gmail.com>
2025-06-12 15:02:38 +0700

    Function subtraction

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,5 +7,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Calculator App");
+        
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"Subtract: {Subtract(a, b)}");
     }
+    
+    static int Subtract(int a, int b) => a - b;
 }
